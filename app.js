55df2605cff087c069608408ed19694120e08821
Kyle Johnson <joh04667@gmail.com>
2016-04-06 17:09:15 -0500

    updates comments

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ var app = angular.module("myApp", []);
 
 app.controller("MainController", function($scope) {
 
-  $scope.tasks = [{text:'wash clothes', done:false},{text:'CSS for this page?', done:false}];
+  // initial array population
+  $scope.tasks = [{text:'wash clothe', done:false},{text:'CSS for this page?', done:false}];
 
+
+// adds task from input to ng-repeated list
   $scope.add = function() {
     if(!$scope.todoInput) {return alert("enter something to do you bum");}
     $scope.tasks.push({text:$scope.todoInput, done:false});
     $scope.todoInput = null;
   };
 
+// repopulates tasks array with only tasks that have .done as true
   $scope.remove = function() {
     var currentList = $scope.tasks;
     $scope.tasks = [];
@@ -17,8 +21,8 @@ app.controller("MainController", function($scope) {
       if (!s.done) {
        $scope.tasks.push(s);
      }
-   });
-  };
+  });
+};
 
   // currentList.forEach(s => !s.done ? $scope.tasks.push(s));  <- why don't you work
 
